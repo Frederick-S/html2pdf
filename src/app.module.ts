61b0e28dbc9988d2cb104f519b4cde5d6285d2db
Frederick-S <mao_xiaodan@hotmail.com>
2020-04-09 14:45:16 +0000

    feature: generate pdf from html

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PdfController } from './pdf.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PdfController],
   providers: [AppService],
 })
 export class AppModule {}
